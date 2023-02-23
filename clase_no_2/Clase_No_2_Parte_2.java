aec36f64c12001d5204be1abc6a421e31a433285
Prisoner's Boy <126206803+JJCCUPC@users.noreply.github.com>
2023-02-23 17:25:07 -0500

    Update Clase_No_2_Parte_2.java

--- clase_no_2/Clase_No_2_Parte_2.java
+++ clase_no_2/Clase_No_2_Parte_2.java
@@ -1,13 +1,39 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package clase_no_2;
 
-/**
- *
- * @author ESTUDIANTE
- */
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Clase_No_2_Parte_2 {
+
     
+    public static void main(String[] args) throws IOException {
+        BufferedReader lea;
+            lea=new BufferedReader(new InputStreamReader(System.in));
+        String nom;
+        
+        //Int b, a, su;
+        float b,a, su;
+/*------------------------------------------------------------------------------*/        
+        System.out.println("Ingrese un nombre: ");
+        nom=lea.readLine();
+        
+        System.out.println("El nombre es: " + nom);        
+        
+/*------------------------------------------------------------------------------*/        
+        try{
+            System.out.println("Ingrese un numero: ");
+            a=Float.parseFloat(lea.readLine() );
+        //Integrer.parseInt
+         System.out.println("Ingrese una numero: ");
+         b=Float.parseFloat(lea.readLine() );
+        //Integrer.parseInt
+         su = a + b;        
+         System.out.println("La suma de a y b es: " + su);
+        }
+/*------------------------------------------------------------------------------*/        
+        catch(NumberFormatException e){
+             System.out.println("Ingrese un numero: ");
+        }
+    }
 }
