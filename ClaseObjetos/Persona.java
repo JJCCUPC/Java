84c3d36b2962a1256beddff9c5637a147fbaea30
Prisoner's Boy <126206803+JJCCUPC@users.noreply.github.com>
2023-03-30 17:49:50 -0500

    Add files via upload

---
+++ ClaseObjetos/Persona.java
@@ -0,0 +1,20 @@
+
+package ClaseObjetos;
+
+public class Persona {
+    public String id;
+     public String apellido;
+      public String nombre;
+       int edad;
+    public Persona(){
+        
+    }
+
+    public Persona(String id, String apellido, String nombre, int edad) {
+        this.id = id;
+        this.apellido = apellido;
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+}
