6cdf7f9de9f66c85661f7c98442f92a711e543af
Prisoner's Boy <126206803+JJCCUPC@users.noreply.github.com>
2023-03-30 17:53:09 -0500

    Add files via upload

---
+++ ClaseObjetos/ClaseObjetos.java
@@ -0,0 +1,20 @@
+
+package ClaseObjetos;
+
+import javax.swing.JOptionPane;
+
+public class ClaseObjetos {
+    
+
+    public static void main(String[] args) {
+       Persona ob = new Persona("117","Collazos","Juan",17);
+       
+       System.out.println("" + ob.id);
+       System.out.println("" + ob.apellido);
+       System.out.println("" + ob.nombre);
+       System.out.println("" + ob.edad);
+       
+      
+    }
+    
+}
---
+++ ClaseObjetos/Persona.java
@@ -0,0 +1,21 @@
+
+package ClaseObjetos;
+
+public class Persona {
+    public String id;
+     public String apellido;
+      public String nombre;
+       int edad;
+       
+    public Persona(){
+        
+    }
+
+    public Persona(String id, String apellido, String nombre, int edad) {
+        this.id = id;
+        this.apellido = apellido;
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+}
